e1963d700ca4585b7f663038415fe32652b2b4c1
Oleksandr Lavrushko <olavrushko@sygic.com>
2022-07-20 16:24:40 +0200

    Fix link to XCFramework

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SygicAuth",
-            url: "https://repository.sygic.com/repository/maven-sygic-releases/com/sygic/lib/auth/sygicauth-ios/1.3.1/sygicauth-ios-1.3.1.zip",
-            checksum: "6be1776ec4554335c0c0e8edf41b2feb55020a538f669deeb255eeaae58230d3")
+            url: "https://public.repo.sygic.com/repository/maven-sygic-releases/com/sygic/lib/auth/sygicauth-ios/1.3.1/sygicauth-ios-1.3.1.zip",
+            checksum: "bdf859e364aff9a66dad6b22eb95d7450b5e21efa68c6be6183cd65f17583e79")
     ]
 )
