7dba446e20412d7f984e82dfb2ced008cd74a4c6
Oleksandr Lavrushko <olavrushkosygic@github.com>
2024-05-03 21:35:00 +0200

    test

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SygicAuth",
-            url: "https://public.repo.sygic.com/repository/maven-sygic-releases/com/sygic/lib/auth/sygicauth-ios/1.4.0/sygicauth-ios-1.4.0.zip",
-            checksum: "566550c1c3fde2f35bf83c5f06f9e70c36ba0ba118015385a7e6d87a108c7c49"
+            url: "test.zip",
+            checksum: "test"
         )
     ]
 )
